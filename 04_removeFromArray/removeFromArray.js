f4b56a2845a6bab38f29f07256e1648f79f7227a
Zack <zack.casparro@pm.me>
2024-05-04 12:58:55 -0400

    Begin work on TOP JS exercise 4

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,6 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(array, toRemove) {
+    cleanArray = array.filter((array) => array != toRemove);
+    return cleanArray;
 };
 
 // Do not edit below this line
