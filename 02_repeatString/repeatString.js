ac62dd82082d106f9f3aed67fc3a2beca6992771
Zack <zack.casparro@pm.me>
2024-05-04 11:50:48 -0400

    Complete TOP JS exercise 2

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,12 @@
-const repeatString = function() {
-
+const repeatString = function(string, num) {
+    if (num < 0) {
+        return 'ERROR'
+} else
+ //   randRepeat = Math.floor(Math.random() * number);
+    return string.repeat(num)
 };
+
+
 
 // Do not edit below this line
 module.exports = repeatString;
