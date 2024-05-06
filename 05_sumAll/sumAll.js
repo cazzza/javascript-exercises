a791d35c8b76602a956539548908d4dc6fdfd54f
Zack <zack.casparro@pm.me>
2024-05-06 16:18:04 -0400

    Complete exercise 5 of TOP JS course

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,19 @@
-const sumAll = function() {
-
+const sumAll = function(a, b) {
+    if ((typeof a === 'number' && typeof b === 'number') && (a >= 0 && b >=0)) {
+        let array = [a,b];
+        array.sort(function(a,b) {return a-b});
+        let smaller = array[0];
+        let bigger = array[1];
+        let numArray = [bigger];
+        while (smaller < bigger) {
+            numArray.push(smaller);
+            smaller++;
+        };
+        let sum = 0;
+        numArray.forEach((item) => sum += item);
+        return sum;
+    } else 
+    return "ERROR";
 };
 
 // Do not edit below this line
