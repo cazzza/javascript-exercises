a5f0fec1d4498d0147e556a54bfceba8e495f2a7
Zack <zack.casparro@pm.me>
2024-05-06 15:50:18 -0400

    Complete TOP JS exercises 04 remove from array

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,7 +1,15 @@
-const removeFromArray = function(array, toRemove) {
-    cleanArray = array.filter((array) => array != toRemove);
+const removeFromArray = function(array, ...toRemove) {
+    cleanArray = array.filter((val) => !toRemove.includes(val));
     return cleanArray;
 };
 
+
+
+//const removeFromArray = function(array, ...toRemove) {
+//    cleanArray = array.filter((array) => array != toRemove);
+//    return cleanArray;
+//};
+
 // Do not edit below this line
 module.exports = removeFromArray;
+
