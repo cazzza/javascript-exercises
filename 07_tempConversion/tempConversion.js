e6fda0795aba02fc46bc34dd52f4d45699f4a98f
Zack <zack.casparro@pm.me>
2024-05-07 10:43:04 -0400

    Complete TOP JS exercise temperatureConversion

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,13 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(temp) {
+  let tempFahrFull = (temp - 32) * (5/9);
+  let tempFahrRounded = Math.round(tempFahrFull * 10) / 10;
+  return tempFahrRounded;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(temp) {
+  let tempCelFull = (temp * (9/5) + 32);
+  let tempCelRounded = Math.round(tempCelFull * 10) / 10;
+  return tempCelRounded;
 };
 
 // Do not edit below this line
