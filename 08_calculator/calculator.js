9e59918c4c3687d51220672c726e2cc30ca7d7c5
Zack <zack.casparro@pm.me>
2024-05-14 12:57:02 -0400

    Complete calculator and being plaindrom exercise

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,42 @@
-const add = function() {
-	
+const add = function(a, b) {
+	return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	return a - b;
 };
 
-const sum = function() {
-	
+const sum = function(array) {
+	return array.reduce(adder, 0);
 };
 
-const multiply = function() {
+function adder(accumulator, a) {
+  return accumulator + a;
+}
 
+
+const multiply = function(numbers) {
+  return numbers.reduce(multiplier, 1);
 };
 
-const power = function() {
-	
+function multiplier(accumulator, a) {
+  return accumulator * a;
 };
 
-const factorial = function() {
-	
+const power = function(a, exp) {
+	let expArray = [];
+  for (let i = 0; i < exp; i++) {
+    expArray.push(a);
+  };
+  return expArray.reduce(multiplier, 1);
+};
+
+const factorial = function(a) {
+	let factArray = [];
+  for (let i = 1; i <= a; i++) {
+    factArray.push(i);
+  };
+  return factArray.reduce(multiplier, 1);
 };
 
 // Do not edit below this line
--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,38 @@
-const palindromes = function () {
+const palindromes = function (string) {
+    // makes string lowercase, turns into array, and creates puncuation array
+    string = string.toLowerCase();
+    const chars = string.split('');
+    const punc = ['.', '!'];
 
+    // compares punctuation array to ending character in target array
+    // removes last character in target array if matches
+    punc.map((item) => item === chars[chars.length - 1] ? chars.splice(chars.length -1, 1) : chars);
+
+
+  // iterates through target array
+  for (let i = 0; i < chars.length; i++) {
+      console.log(`Comparing ${chars[i]} and ${chars[chars.length - 1 - i]}`);
+      if (chars[i] !== chars[chars.length - 1 - i]) {
+        return false;
+      }
+  }
+
+return true;
 };
 
 // Do not edit below this line
 module.exports = palindromes;
+
+
+
+
+
+//function checkPalindrome(string) {
+//    const len = string.length;
+//    for (let i = 0; i < len / 2; i++) {
+//        if (string[i] !== string[len - 1 - i]) {
+//            return 'It is not a palindrome';
+//        }
+//    }
+//    return 'It is a palindrome';
+//}
