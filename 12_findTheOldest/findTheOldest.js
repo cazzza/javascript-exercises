4b8dc90dddf0d70b68ab35cfef429a88e2b7802c
Zack <zack.casparro@pm.me>
2024-05-15 10:14:01 -0400

    Complete findTheOldest TOP JS exercise

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,24 +1,44 @@
-const findTheOldest = function(people) {
-    
-    if (people.yearOfDeath === 'undefined') {
-        people.yearOfDeath = date.getFullYear();
+const getAge = function (birth, death) {
+    if (!death) {
+      death = new Date().getFullYear();
     }
+    return death - birth;
+  };
+  
+  const findTheOldest = function (people) {
+    return people.reduce((oldest, currentPerson) => {
+      const oldestAge = getAge(oldest.yearOfBirth, oldest.yearOfDeath);
+      const currentAge = getAge(
+        currentPerson.yearOfBirth,
+        currentPerson.yearOfDeath
+      );
+      return oldestAge < currentAge ? currentPerson : oldest;
+    });
+  };
 
-    people.forEach(person => person.age = person.yearOfDeath - person.yearOfBirth)
-    console.table(people);
-    
-    const ages = [];
-    people.forEach((person) => ages.push(person.age));
-    ages.sort((a,b) => {return a-b});
-    console.log(ages);
-    const oldestAge = ages[ages.length - 1];
-    console.log(oldestAge);
-    console.log(people);
+// This is my code that doesn't seem to work.
 
-    const oldestPerson = people.filter((person) => person.age === oldestAge);
-    console.log(oldestPerson.name);
-    return oldestPerson.name
-};
+// const findTheOldest = function(people) {
+//     
+//     if (people.yearOfDeath === 'undefined') {
+//         people.yearOfDeath = new Date.getFullYear();
+//     }
+// 
+//     people.forEach(person => person.age = person.yearOfDeath - person.yearOfBirth)
+//     console.table(people);
+//     
+//     const ages = [];
+//     people.forEach((person) => ages.push(person.age));
+//     ages.sort((a,b) => {return a-b});
+//     console.log(ages);
+//     const oldestAge = ages[ages.length - 1];
+//     console.log(oldestAge);
+//     console.log(people);
+// 
+//     const oldestPerson = people.filter((person) => person.age === oldestAge);
+//     console.log(oldestPerson[0].name);
+//     return oldestPerson[0].name
+// };
 
 // Do not edit below this line
 module.exports = findTheOldest;
